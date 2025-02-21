7c4e77973fa717d46e927e36ceb91c193ca17f43
Billy <Billy>
2025-02-21 23:42:03 +0200

    3rdtask

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 words = ['Sky',
 'Tree',
 'Moon',
@@ -9,13 +11,13 @@ words = ['Sky',
 'Ocean',
 'Peace']
 
-import random
+
 
 word = random.choice(words)
 n_word = '_ ' * len(word)
+print(n_word)
 
 guess = input("Bir harf tahmin edin: ").upper()
 while len(guess) != 1 or not guess.isalpha():
     guess = input("Lütfen tek bir harf girin: ").upper()
-
-print(n_word)
+print(f"Tahmininiz :{guess}")
