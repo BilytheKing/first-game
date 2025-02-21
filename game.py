ca39992a87016aa0466c8033ba01f3351002f252
Billy <Billy>
2025-02-21 22:54:15 +0200

    3rdtask

--- game.py
+++ game.py
@@ -14,4 +14,8 @@ import random
 word = random.choice(words)
 n_word = '_ ' * len(word)
 
+guess = input("Bir harf tahmin edin: ").upper()
+while len(guess) != 1 or not guess.isalpha():
+    guess = input("Lütfen tek bir harf girin: ").upper()
+
 print(n_word)
